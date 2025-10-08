1c8405ac103a7c054b4af34b10ffbcf58c4e6f87
Danilo Silva <danilosilva441@gmail.com>
2025-10-07 23:33:57 -0300

    altera

--- backend/AuthService/obj/Debug/net8.0/AuthService.AssemblyInfo.cs
+++ backend/AuthService/obj/Debug/net8.0/AuthService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuthService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07ef0c15bea354107cb8bfd06d5b4d41ff98f0f7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4bf6fef4c06a0412a5129674ceba969d148dc14")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuthService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuthService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
