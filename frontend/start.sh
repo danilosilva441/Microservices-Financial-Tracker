b789f59960cac5b9ee7226abe043d95b5fecde21
Danilo Silva <danilosilva441@gmail.com>
2025-10-11 15:41:56 -0300

    st

--- frontend/start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# Gera o arquivo final de configuração do Nginx a partir do template
-envsubst < /etc/nginx/conf.d/nginx.template > /etc/nginx/conf.d/default.conf
-
-# Inicia o nginx em primeiro plano
-nginx -g 'daemon off;'
